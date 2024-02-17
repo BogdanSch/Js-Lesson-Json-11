51da9eaf0212b58999d4f8cc03d6d9986592ca84
Bohdan <bogmira777@gmail.com>
2024-02-17 10:26:39 +0100

    Students task fix and optimization

--- Lesson1/grades/js/script.js
+++ Lesson1/grades/js/script.js
@@ -1,6 +1,9 @@
 const tableContainer = document.getElementById("studentsContainer");
-const submitButton = document.getElementById("submitButton");
+const averageGroupScoreContainer = document.querySelector(
+  "#averageGroupScore span"
+);
 
+const submitButton = document.getElementById("submitButton");
 const studentsForm = document.forms["marks"];
 
 let students = localStorage.getItem("students")
@@ -20,17 +23,19 @@ function saveMarks(event) {
     PHP: parseFloat(studentsForm.m2.value),
     JavaScript: parseFloat(studentsForm.m3.value),
   };
+  let studentAverage = calculateAverageObject(marks);
 
   let student = {
     name: name,
     marks: marks,
+    studentAverage: studentAverage,
   };
 
   students = localStorage.getItem("students")
     ? JSON.parse(localStorage.getItem("students"))
     : [];
   students.push(student);
-  console.log(students);
+  // console.log(students);
   localStorage.setItem("students", JSON.stringify(students));
   console.log(JSON.parse(localStorage.getItem("students")));
   displayStudents();
@@ -41,22 +46,43 @@ function displayStudents() {
   tableContainer.innerHTML = "";
 
   if (students.length) {
-    students.forEach(function (student) {
+    students.forEach((student) => {
       let row = "<tr>";
       row += "<td>" + student.name + "</td>";
       row += "<td>" + student.marks.Java + "</td>";
       row += "<td>" + student.marks.PHP + "</td>";
       row += "<td>" + student.marks.JavaScript + "</td>";
-      row += "<td>" + calculateAverage(student.marks) + "</td>";
+      row += "<td>" + student.studentAverage + "</td>";
       row += "</tr>";
       tableContainer.innerHTML += row;
     });
   }
+  displayStudentAverages();
 }
 
-function calculateAverage(marks) {
+function displayStudentAverages() {
+  let allStudentAverage = [];
+  if (students.length) {
+    students.forEach((student) => {
+      allStudentAverage.push(parseFloat(student.studentAverage));
+    });
+  }
+  averageGroupScoreContainer.innerHTML =
+    calculateAverageArray(allStudentAverage);
+}
+
+function calculateAverageObject(marks) {
   let sum = Object.values(marks).reduce((acc, val) => acc + val, 0);
   return (sum / Object.keys(marks).length).toFixed(2);
+}
+
+function calculateAverageArray(marks) {
+  if (marks.length) {
+    console.log(marks);
+    let sum = marks.reduce((acc, val) => acc + val, 0);
+    return (sum / marks.length).toFixed(2);
+  }
+  return 0;
 }
 
 displayStudents();
